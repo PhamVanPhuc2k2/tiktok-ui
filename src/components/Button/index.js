a31c93679d3163f6619f7c359729fb6c8fcadfbc
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-02-24 22:19:09 +0700

    Xây dựng UI phần Header #3

---
+++ src/components/Button/index.js
@@ -0,0 +1,67 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    rounded,
+    disabled,
+    text,
+    primary,
+    large,
+    small,
+    outline,
+    onClick,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    ...passProps
+}) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof [key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        small,
+        large,
+        text,
+        disabled,
+        rounded,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
