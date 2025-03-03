6afbe5234b99701836cfc9b7e878a2ce95d83cd9
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-03-03 13:02:23 +0700

    Xây dựng Logic phần Header  #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -6,6 +6,7 @@ import { SearchIcon } from '~/components/Icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { useEffect, useState, useRef } from 'react';
+import useDebounce from '~/hooks/useDebounce';
 
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
@@ -18,17 +19,19 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -37,7 +40,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');
---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+import { default as useDebugValue } from 'react';
